9a37721b7c6a1f2e661248cf49fd156a0f1150de
unknown <G>
2022-01-11 23:11:25 +1100

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import {Route} from 'react-router-dom';
+import {Switch} from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
     </div>
   );
 }
